24c0910c98a0bcc863ef5f19be6ebb3e04876a5b
nyadiia <nyadiia@pm.me>
2024-08-17 23:05:04 -0500

    feat: transparent background on oxocarbon

--- modules/theme/supported_themes.nix
+++ modules/theme/supported_themes.nix
@@ -40,6 +40,8 @@ in
         -- oxocarbon theme
         vim.opt.background = "${cfg.style}"
         vim.cmd.colorscheme "oxocarbon"
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
       '';
       styles = [
         "dark"
